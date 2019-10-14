3d4de58b96fb001c6a907547f4305f8b49a874b9
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-13 20:49:30 -0400

    finalized war game

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+#War Card Game
+This project was based upon creating a Javascript console card game, specifically the card game _I Declare War_**.
+
+##Getting Started 
+Below you will see how my game came to be. In order to view and/or contribute you will need something to do so, which will be explained below. 
+
+##Prerequisites 
+Thing(s) that you need to install un
---
+++ War4.js
@@ -0,0 +1,75 @@
+class Deck {
+    constructor() {
+      this.deck = [];
+      this.player1 = [];
+      this.player2 = [];
+      this.shuffle()
+  
+      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+      const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]} of ${suits[suit]}`);
+        }
+      }
+    }
+      shuffle() {
+      const { deck } = this;
+      let m = deck.length, i;
+  
+      while (m) {
+        i = Math.floor(Math.random() * m--);
+  
+        [deck[m], deck[i]] = [deck[i], deck[m]];
+      }
+  
+      return this;
+      }
+    
+    deal() {
+      this.player1 = this.deck.slice(0,26)
+      this.player2 = this.deck.slice(26,52)
+  
+      return this.player1, this.player2
+    }
+    playWar() {
+        deck1.length = 52;
+        while (deck1.length > 0){
+        let playCard1 = this.player1.shift()
+              console.log(playCard1)
+              let playCard2 = this.player2.shift()
+              console.log(playCard2)
+              if (playCard1 > playCard2){
+                  this.player1.push(playCard1, playCard2)
+                  console.log ('Player 1 the real winner!')
+              }
+              else if (playCard2 > playCard1) {
+                  this.player2.push(playCard1, playCard2)
+                  console.log ('Player 2 wins!')
+              }
+              else {
+                  console.log ('WAR IS COMING!')
+              }
+              deck1.length--;
+          }
+      }
+      endGame() {
+          if (deck1.player1.length > deck1.player2.length) {
+              console.log(`Player 1 got ${deck1.player1.length} cards, and player 2 has ${deck1.player2.length} cards`)
+              console.log('Player 1 went crazy on player 2 OMG')
+          }
+          else {
+              console.log(`Player 2 got ${deck1.player2.length} cards, and player 1 has ${deck1.player1.length} cards`)
+              console.log('Player 2 wilded on player 1')
+          }
+      }
+    }
+  
+  
+  const deck1 = new Deck();
+  
+  deck1.shuffle();
+  deck1.deal();
+  deck1.playWar();
+  deck1.endGame();
